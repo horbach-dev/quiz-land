4ca2dc8d94aef5e96d043f7a4cbafe9ec291b6b7
horbach_ux <horbach.ux@gmail.com>
2025-12-02 21:56:05 +0300

    build

--- client/src/features/quiz/services/useQuizListQuery.ts
+++ client/src/features/quiz/services/useQuizListQuery.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { getQuizList } from "../api/get-quiz-list";
+import mock from './mock.json'
 
 type TQuizListParams = {
   type: 'friends' | 'my' | 'public'
@@ -9,5 +10,8 @@ export function useQuizListQuery (params?: TQuizListParams) {
   return useQuery({
     queryKey: ['getQuizList', params],
     queryFn: () => getQuizList(params),
+    placeholderData: () => {
+      return [{ ...mock, id: '1' }, { ...mock, id: '2' }, { ...mock, id: '3' }, { ...mock, id: '4' }]
+    }
   })
 }
--- client/src/features/quiz/services/useQuizQuery.ts
+++ client/src/features/quiz/services/useQuizQuery.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { getQuiz } from "../api/get-quiz";
 import { BASE_URL } from "@/constants";
+import mock from './mock.json'
+import type { TQuiz } from "@/features/quiz/types.ts";
 
 export function useQuizQuery (id: string) {
   return useQuery({
@@ -12,5 +14,10 @@ export function useQuizQuery (id: string) {
         poster: BASE_URL + data.poster,
       }
     },
+    placeholderData: (): TQuiz => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      return mock
+    }
   })
 }
