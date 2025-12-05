98a64eb2c5a9e6273a32a4639681f6b694e67efd
horbach_ux <horbach.ux@gmail.com>
2025-12-05 16:31:32 +0300

    build

---
+++ client/src/features/quiz/components/QuizList/QuizList.tsx
@@ -0,0 +1,56 @@
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import type { TQuiz } from "@/features/quiz/types";
+import { QuizCardSkeleton } from "@/features/quiz/components/QuizCard/quiz-card-skeleton";
+import { IntersectingWrapper } from "@/shared/components/IntersectingWrapper";
+import styles from './QuizList.module.css';
+
+interface IProps {
+  view?: 'column' | 'row';
+  data: TQuiz[] | undefined;
+  isLoading?: boolean;
+  isLoadingMore?: boolean;
+  handleLoadMore?: () => void;
+  renderItem: (data: TQuiz) => ReactNode
+}
+
+export const QuizList = ({
+  view = 'column',
+  data,
+  handleLoadMore,
+  isLoading,
+  isLoadingMore,
+  renderItem
+}: IProps) => {
+  const isShowSkeleton = (isLoading && !data?.length) || isLoadingMore;
+
+  return (
+    <>
+      <div className={clsx(styles.container, styles[`container-${view}`])}>
+        {data?.map(renderItem)}
+        {isShowSkeleton && (
+          <>
+            <QuizCardSkeleton view={view} />
+            <QuizCardSkeleton view={view} />
+            <QuizCardSkeleton view={view} />
+            <QuizCardSkeleton view={view} />
+          </>
+        )}
+      </div>
+      {!data?.length && !isLoading && (
+        <div className={styles.empty}>
+          <div className={styles.emptyImage} />
+          <p className={styles.emptyTitle}>Ничего не найдено</p>
+        </div>
+      )}
+      {(
+        handleLoadMore &&
+        !isLoading &&
+        !isLoadingMore &&
+        !!data?.length
+      ) && (
+        <IntersectingWrapper onVisible={handleLoadMore} />
+      )}
+    </>
+  )
+}
